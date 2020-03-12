b5bcecf0bc5981a2d367cb6ce9c0f17864c63935
Frode Hansen <frode.hansen@bekk.no>
2020-03-12 09:19:40 +0100

    fisker_er_pa_blad_b

--- src/forms/virksomhet/mapVirksomhetToApiData.ts
+++ src/forms/virksomhet/mapVirksomhetToApiData.ts
@@ -49,7 +49,7 @@ export const mapVirksomhetToVirksomhetApiData = (virksomhet: Virksomhet): Virkso
     }
 
     if (harFiskerNæringstype(virksomhet.næringstyper) && virksomhet.fiskerErPåBladB) {
-        data.fisker_er_paa_blad_b = virksomhet.fiskerErPåBladB === YesOrNo.YES;
+        data.fisker_er_pa_blad_b = virksomhet.fiskerErPåBladB === YesOrNo.YES;
     }
 
     if (virksomhet.harBlittYrkesaktivILøpetAvDeTreSisteFerdigliknedeÅrene) {
--- src/forms/virksomhet/types.ts
+++ src/forms/virksomhet/types.ts
@@ -69,7 +69,7 @@ export const isVirksomhet = (virksomhet: Partial<Virksomhet>): virksomhet is Vir
 
 export interface VirksomhetApiData {
     naringstype: Næringstype[];
-    fisker_er_paa_blad_b?: boolean;
+    fisker_er_pa_blad_b?: boolean;
     fra_og_med: ApiStringDate;
     til_og_med?: ApiStringDate | null;
     er_pagaende?: boolean;
