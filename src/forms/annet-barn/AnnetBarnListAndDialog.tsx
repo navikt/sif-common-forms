e8f81b176becb9635709ed97cb48c39cee065046
Aleksei Poroshin <aleksei.poroshin@nav.no>
2020-09-09 14:36:06 +0200

    Lagt til dato validering

--- src/forms/annet-barn/AnnetBarnListAndDialog.tsx
+++ src/forms/annet-barn/AnnetBarnListAndDialog.tsx
@@ -8,9 +8,11 @@ interface Props<FieldNames> {
     name: FieldNames;
     validate?: FormikValidateFunction;
     labels: ModalFormAndListLabels;
+    minDate: Date;
+    maxDate: Date;
 }
 
-function AnnetBarnListAndDialog<FieldNames>({ name, validate, labels }: Props<FieldNames>) {
+function AnnetBarnListAndDialog<FieldNames>({ name, validate, labels, minDate, maxDate }: Props<FieldNames>) {
     return (
         <>
             <FormikModalFormAndList<FieldNames, AnnetBarn>
@@ -19,7 +21,13 @@ function AnnetBarnListAndDialog<FieldNames>({ name, validate, labels }: Props<Fi
                 dialogWidth="narrow"
                 validate={validate}
                 formRenderer={({ onSubmit, onCancel, item }) => (
-                    <AnnetBarnForm annetBarn={item} onSubmit={onSubmit} onCancel={onCancel} />
+                    <AnnetBarnForm
+                        annetBarn={item}
+                        onSubmit={onSubmit}
+                        onCancel={onCancel}
+                        minDate={minDate}
+                        maxDate={maxDate}
+                    />
                 )}
                 listRenderer={({ items, onEdit, onDelete }) => (
                     <AnnetBarnList annetBarn={items} onEdit={onEdit} onDelete={onDelete} />
