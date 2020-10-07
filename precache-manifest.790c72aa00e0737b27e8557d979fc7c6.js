77d25aa863b172272bf73522292f22adcdf2f24e
Frode Hansen <frode.hansen@bekk.no>
2020-10-07 10:15:24 +0200

    Updates

--- precache-manifest.790c72aa00e0737b27e8557d979fc7c6.js
+++ precache-manifest.790c72aa00e0737b27e8557d979fc7c6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a0f947058fd8b0174fb4bb7892d4e49a",
+    "revision": "b89721a37faf6dd59f4a5661c9f5f215",
     "url": "/sif-common-forms/index.html"
   },
   {
-    "revision": "d7c3883af5fedc52fc2a",
+    "revision": "8eedea8cf8da53c129b4",
     "url": "/sif-common-forms/static/css/2.30a6d748.chunk.css"
   },
   {
-    "revision": "35b0d2d1afa85c674839",
+    "revision": "e2ef6faeeac1490b39e5",
     "url": "/sif-common-forms/static/css/main.c68890b2.chunk.css"
   },
   {
-    "revision": "d7c3883af5fedc52fc2a",
-    "url": "/sif-common-forms/static/js/2.2237b383.chunk.js"
+    "revision": "8eedea8cf8da53c129b4",
+    "url": "/sif-common-forms/static/js/2.bf7cea1b.chunk.js"
   },
   {
     "revision": "94fb3ff2a63140a838ab75fa2d5fa873",
-    "url": "/sif-common-forms/static/js/2.2237b383.chunk.js.LICENSE.txt"
+    "url": "/sif-common-forms/static/js/2.bf7cea1b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "35b0d2d1afa85c674839",
-    "url": "/sif-common-forms/static/js/main.02fcd476.chunk.js"
+    "revision": "e2ef6faeeac1490b39e5",
+    "url": "/sif-common-forms/static/js/main.017bfcfa.chunk.js"
   },
   {
     "revision": "e648366ae66ac1f8efcd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sif-common-forms/precache-manifest.bae216f5d12ca231466e021c35cbe769.js"
+  "/sif-common-forms/precache-manifest.790c72aa00e0737b27e8557d979fc7c6.js"
 );
 
 self.addEventListener('message', (event) => {
