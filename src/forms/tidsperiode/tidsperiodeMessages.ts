03cd7dc403a9912ef020d688c16039f681ee8427
Frode Hansen <frode.hansen@bekk.no>
2020-10-23 08:11:20 +0200

    Update nynorsk intl texts

--- src/forms/tidsperiode/tidsperiodeMessages.ts
+++ src/forms/tidsperiode/tidsperiodeMessages.ts
@@ -12,14 +12,14 @@ const tidsperiodeMessages = {
     },
     nn: {
         'tidsperiode.form.title': 'Tidsperiode',
-        'tidsperiode.form.fromDate': 'Fra og med',
+        'tidsperiode.form.fromDate': 'Frå og med',
         'tidsperiode.form.toDate': 'Til og med',
         'tidsperiode.form.okButton': 'Ok',
         'tidsperiode.form.cancelButton': 'Avbryt',
         'tidsperiode.form.validation.required': 'Feltet er påkrevd',
         'tidsperiode.form.validation.dateOutsideRange': 'Første gyldige dato er {fom}, og siste gyldige dato er {tom}',
-        'tidsperiode.form.validation.fromDateAfterToDate': 'Fra-dato må være lik eller før til-dato',
-        'tidsperiode.form.validation.toDateBeforeFromDate': 'Til-dato må være lik eller etter fra-dato',
+        'tidsperiode.form.validation.fromDateAfterToDate': 'Frå-datoen må vere lik eller før til-datoen',
+        'tidsperiode.form.validation.toDateBeforeFromDate': 'Til-datoen må vere lik eller etter frå-dato',
     },
 };
 
