e640079f1c821c5118f69dae8fbea018a62a93bf
Aleksei Poroshin <aleksei.poroshin@nav.no>
2021-03-26 15:38:47 +0100

    Endret nynorsk i annetBarnMessages (nn)

--- src/forms/annet-barn/annetBarnMessages.ts
+++ src/forms/annet-barn/annetBarnMessages.ts
@@ -11,12 +11,12 @@ const annetBarnMessages = {
     },
     nn: {
         'annetBarn.form.title': 'Legg til barn',
-        'annetBarn.form.fnr': 'Barnets fødselsnummer/D-nummer',
-        'annetBarn.form.fødselsdato': 'Barnets fødselsdato',
-        'annetBarn.form.navn': 'Barnets navn',
+        'annetBarn.form.fnr': 'Fødselsnummeret/D-nummeret til barnet',
+        'annetBarn.form.fødselsdato': 'Fødselsdatoen til barnet',
+        'annetBarn.form.navn': 'Namnet til barnet',
         'annetBarn.form.okButton': 'Ok',
         'annetBarn.form.cancelButton': 'Avbryt',
-        'annetBarn.form.validation.required': 'Feltet er påkrevd',
+        'annetBarn.form.validation.required': 'Feltet er påkravd.',
         'annetBarn.list.født': 'Født',
     },
 };
